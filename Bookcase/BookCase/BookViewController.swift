ec0f9766a3c8234501b618177e865eec2741d941
Nikki <Nikkisali9@gmail.com>
2019-11-06 14:25:29 -0700

    Scroll views

--- Bookcase/BookCase/BookViewController.swift
+++ Bookcase/BookCase/BookViewController.swift
@@ -1,10 +1,6 @@
 //
 //  BookViewController.swift
 //  BookCase
-//
-//  Created by Marzieh on 2019-10-31.
-//  Copyright © 2019 Myph. All rights reserved.
-//
 
 import UIKit
 
@@ -12,8 +8,8 @@ class BookViewController: UIViewController {
     
     @IBOutlet weak var bookCover: UIImageView!
     @IBOutlet weak var isbnStackView: UIStackView!
-    @IBOutlet weak var topConstraint: NSLayoutConstraint!
-    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var outerStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,31 +17,20 @@ class BookViewController: UIViewController {
         // you can put an infoButton and make the isbn isHidden here
     }
     
+    
     @objc func keyboardFrameChanges(notification:Notification) {
          //@objc is because of #selector
         
-        guard let userInfo = notification.userInfo else { return }
-        guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
-        guard let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
-        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        guard let userInfo = notification.userInfo,
+        let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {return}
+        let offset = self.view.frame.height - keyboardFrame.origin.y // this is to define the which is how much it should go up, which is the height of the root view - the height of the keyboard
         
-        var offset:CGFloat = 0
-        if let firstResponder = firstResponder {
-            let frFrame = self.view.convert(firstResponder.frame, from: firstResponder) // because it was in stackview, we have to convert it to the root view coordinates
-            let frMaxY = frFrame.maxY // the max y coordinate of the first responder frame
-            if frMaxY > keyboardFrame.origin.y {
-                offset = frMaxY - keyboardFrame.origin.y
-            }
+        scrollView.contentInset.bottom =  offset
+        scrollView.verticalScrollIndicatorInsets.bottom = offset
+        if let textView = firstResponder as? UITextView, let textViewSuperview = textView.superview { // reference to the stackview and its superview
+            let textViewFrame =  outerStackView.convert(textView.frame, from: textViewSuperview) // convert the coordinates
+            scrollView.scrollRectToVisible(textViewFrame, animated: true)
         }
-        UIView.animate(withDuration:duration,
-                       delay: 0,
-                       options: UIView.AnimationOptions(rawValue: curve),
-                       animations: {
-                        self.topConstraint.constant = -offset //negative direction
-                        self.bottomConstraint.constant = offset
-                        self.view.layoutIfNeeded()
-        },
-                       completion: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -56,13 +41,6 @@ class BookViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) { //when scene is no longer active, we want the viewController to stop recieving notifications
         super.viewDidDisappear(animated)
         NotificationCenter.default.removeObserver(self)
-    }
-    
-    
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
-        view.endEditing(true) //goes on a hunt on its subview hierarchy to find the first responder, then requests it to resign
     }
 }
 
