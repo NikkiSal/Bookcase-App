db7af72d52c3b61d251364f404dc6cb3a09395dd
Nikki <Nikkisali9@gmail.com>
2019-11-19 18:02:10 -0700

    changing the sort order

--- Bookcase/BookCase/BookCase/BookManager.swift
+++ Bookcase/BookCase/BookCase/BookManager.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 
+enum SortOrder: Int {
+    case title
+    case author
+    
+}
+
 class BooksManager {
     
     private lazy var books = loadBooks() // lazy stored property because it's an instance method
+    var sortOrder:SortOrder = .title {
+        didSet {
+            sort(books: &books)
+        }
+    }
+    
     var bookCount:Int {
         return books.count
     }
@@ -23,10 +35,12 @@ class BooksManager {
     
     func addBook(_ book:Book) {
         books.append(book)
+        sort(books: &books)
     }
     
     func updateBook(at index:Int, with book:Book) {
         books[index] = book
+        sort(books: &books)
     }
     
     func removeBooks(at index:Int) {
@@ -34,12 +48,27 @@ class BooksManager {
     }
     
     private func sampleBooks() ->[Book] {
-        return [
+        var books = [
             Book(title: "Great Expectations", author: "Charles Dickes", rating: 5, isbn: "97454545345", notes: " gift from papa"),
             Book(title: "Don Quixote", author: "Miquel De Cervantes", rating: 4, isbn: "32344534534", notes: ""),
             Book(title: "Harry Potter", author: "J.K. Rowling", rating: 5, isbn: "345345345", notes: "Best book ever"),
             Book(title: "Gulliver's Travels", author: "Jonathan Swift", rating: 5, isbn: "", notes: "")
         ]
-        
+        sort(books: &books)
+        return books
+    }
+    func sort(books:inout [Book]) {
+        switch sortOrder {
+        case .title:
+            books.sort {
+                return ($0.title.localizedLowercase, $1.author.localizedLowercase) < ($1.title.localizedLowercase, $1.author.localizedLowercase)
+            }
+        case .author:
+            books.sort {
+                return ($0.author.localizedLowercase, $1.title.localizedLowercase) < ($1.author.localizedLowercase, $1.title.localizedLowercase)
+            }
+        default:
+            print("do something else")
+        }
     }
 }
--- Bookcase/BookCase/BookCase/BooksTableViewController.swift
+++ Bookcase/BookCase/BookCase/BooksTableViewController.swift
@@ -110,6 +110,14 @@ class BooksTableViewController: UITableViewController {
         
     }
     
+    @IBAction func changedSegment(_ sender: UISegmentedControl) {
+        guard let sortOrder = SortOrder(rawValue: sender.selectedSegmentIndex)
+            else {return}
+        booksManager.sortOrder = sortOrder
+        tableView.reloadData()
+    }
+    
+    
 }
 
 extension BooksTableViewController:BookViewControllerDelegate {
